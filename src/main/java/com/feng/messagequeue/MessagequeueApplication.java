17d5a58f0c5f5c2a81f84dc91f492d0079543cc7
Charles Phan <phanvanluom97bd@gmail.com>
2024-04-22 23:50:53 +0700

    Add GitHub Actions workflow for building and publishing package

--- src/main/java/com/feng/messagequeue/MessagequeueApplication.java
+++ src/main/java/com/feng/messagequeue/MessagequeueApplication.java
@@ -2,8 +2,9 @@ package com.feng.messagequeue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.feng.messagequeue")
 public class MessagequeueApplication {
 
     public static void main(String[] args) {
